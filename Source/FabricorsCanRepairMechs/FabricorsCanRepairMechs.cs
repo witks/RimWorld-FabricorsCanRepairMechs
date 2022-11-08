4b77a76554fb3b68425d5077150b2715fd33c14a
Mateusz Graj <mateusz@graj.cc>
2022-11-08 20:12:54 +0100

    first commit

---
+++ Source/FabricorsCanRepairMechs/FabricorsCanRepairMechs.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Verse;
+using System.Reflection;
+
+namespace FabricorsCanRepairMechs
+{
+    [StaticConstructorOnStartup]
+    public static class FabricorsCanRepairMechs
+    {
+        static FabricorsCanRepairMechs()
+        {
+            Harmony harmony = new Harmony(id: "witek.FabricorsCanRepairMechs");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Source/FabricorsCanRepairMechs/WorkGiver_RepairMech_ShouldSkip_Patch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace FabricorsCanRepairMechs
+{
+    [HarmonyPatch(typeof(WorkGiver_RepairMech), "ShouldSkip")]
+    public static class WorkGiver_RepairMech_ShouldSkip_Patch
+    {
+        public static bool Prefix(ref bool __result, Pawn pawn, bool forced = false)
+        {
+            bool result = true;
+            bool ismech = pawn.RaceProps.IsMechanoid;
+            bool damaged = MechRepairUtility.CanRepair(pawn);
+            if (ismech && !damaged)
+            {
+                __result = false;
+                result = false;
+            }
+            return result;
+        }
+    }
+
+    [HarmonyPatch(typeof(WorkGiver_RepairMech), "HasJobOnThing")]
+    public static class WorkGiver_RepairMech_HasJobOnThing_Patch
+    {
+        public static bool Prefix(ref bool __result, Pawn pawn, Thing t, bool forced = false)
+        {
+            bool result = true;
+            Pawn pawn2 = (Pawn)t;
+            if (pawn == pawn2)
+            {
+                __result = false;
+                result = false;
+            }
+            return result;
+        }
+    }
+}
